ad441de688dc77b3191c39a8cbceaa4862ac6369
Developer <dev@reflexiq.local>
2026-01-23 03:10:38 +0100

    util: add debounce-throttle utility

---
+++ src/lib/utils/debounce-throttle.ts
@@ -0,0 +1,24 @@
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout;
+  return function (...args: Parameters<T>) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  };
+}
+
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let lastRun = 0;
+  return function (...args: Parameters<T>) {
+    const now = Date.now();
+    if (now - lastRun >= limit) {
+      func(...args);
+      lastRun = now;
+    }
+  };
+}
